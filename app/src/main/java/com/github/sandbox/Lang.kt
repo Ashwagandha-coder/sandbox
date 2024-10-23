188a4187a421b188f1eb7576834b9e517408be2a
Ashwagandha-coder <max.cacz@yandex.ru>
2024-10-23 13:18:51 +0500

    add new value classes

--- app/src/main/java/com/github/sandbox/Lang.kt
+++ app/src/main/java/com/github/sandbox/Lang.kt
@@ -1,8 +1,29 @@
 package com.github.sandbox
 
+
+/**
+ * Example of use value class
+ */
+
+
 @JvmInline
 value class Email(val value: String) {
     init {
         require(value.contains("@")) { "Invalid email format" }
     }
+}
+
+data class Dimensions(val width: Int, val height: Int)
+
+@JvmInline
+value class ScreenDimensions(val dimen: Dimensions)
+
+@JvmInline
+value class Percentage(val value: Int) {
+    init {
+        require(value in 0..100) {
+            "Percentage value must be between 0 and 100"
+        }
+    }
+
 }
