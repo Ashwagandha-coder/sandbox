a95845879d96a55082912b044510d2682fa9d242
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-13 22:07:59 +0500

    and infix

--- kotlinFeatures/src/main/java/com/github/kotlinfeatures/Execution.kt
+++ kotlinFeatures/src/main/java/com/github/kotlinfeatures/Execution.kt
@@ -1,5 +1,5 @@
 package com.github.kotlinfeatures
 
 fun main() {
-    something("")
+    testingMultiple()
 }
--- kotlinFeatures/src/main/java/com/github/kotlinfeatures/KotlinSandBox.kt
+++ kotlinFeatures/src/main/java/com/github/kotlinfeatures/KotlinSandBox.kt
@@ -4,4 +4,28 @@ fun something(string: String): Boolean {
     return string.isEmpty()
 }
 
+fun testingMultiple() {
 
+    val a = 5
+    val b = 3
+    println(a multiple b)
+    println(a substraction b)
+    println(a divide b)
+    println(a sum b)
+
+}
+
+infix fun Int.substraction(number: Int): Int = this - number
+
+
+infix fun Int.divide(number: Int): Int {
+    return this / number
+}
+
+infix fun Int.sum(number: Int): Int {
+    return this + number
+}
+
+infix fun Int.multiple(number: Int): Int {
+    return this * number
+}
