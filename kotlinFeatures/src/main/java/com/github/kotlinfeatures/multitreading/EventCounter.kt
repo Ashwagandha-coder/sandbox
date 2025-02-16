8ac0d9250d7e44b54a5377d2d481d20c6bdbdab4
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-16 21:46:31 +0500

    add event counter thread - safety

---
+++ kotlinFeatures/src/main/java/com/github/kotlinfeatures/multitreading/EventCounter.kt
@@ -0,0 +1,21 @@
+package com.github.kotlinfeatures.multitreading
+
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
+class EventCounter {
+
+    private var count = 0
+    private val lock = ReentrantLock()
+
+
+    fun increment() = lock.withLock {
+        count++
+    }
+
+    fun decrement() = lock.withLock {
+        count--
+    }
+
+
+}
