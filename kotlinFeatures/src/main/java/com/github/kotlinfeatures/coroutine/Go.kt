4b6f47091dacf27d8924a29f12f6343aef7484a9
Ashwagandha-coder <max.cacz@yandex.ru>
2025-02-21 00:15:40 +0500

    add little code for coroutine

--- kotlinFeatures/src/main/java/com/github/kotlinfeatures/coroutine/Go.kt
+++ kotlinFeatures/src/main/java/com/github/kotlinfeatures/coroutine/Go.kt
@@ -1,5 +1,8 @@
 package com.github.kotlinfeatures.coroutine
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.yield
@@ -7,6 +10,20 @@ import kotlinx.coroutines.yield
 fun main() {
 
 
+    runBlocking {
+        val windows = async(Dispatchers.IO) { order(Product.Windows) }
+        val doors = async(Dispatchers.IO) { order(Product.Doors) }
+        launch(Dispatchers.Default) {
+            perform("laying bricks")
+            launch { perform("installing ${windows.await().description}") }
+            launch { perform("installing ${doors.await().description}") }
+        }
+    }
+
+}
+
+
+fun saveBaseCode() {
     runBlocking {
         launch {
             println("Hammer: Clothesline!")
@@ -29,4 +46,22 @@ suspend fun tagOut() {
     println("Tag Out")
     yield()
 
+}
+
+enum class Product(val description: String, val deliveryTime: Int) {
+    Windows("windows", 32), Doors("doors", 56)
+}
+
+suspend fun order(item: Product): Product {
+    println("ORDER EN ROUTE  >>> The ${item.description} are on the way!")
+    delay(item.deliveryTime.toLong())
+    println("ORDER DELIVERED >>> Your ${item.description} have arrived.")
+    return item
+
+}
+
+suspend fun perform(taskName: String) {
+    println("STARTING TASK   >>> $taskName")
+    Thread.sleep(1_000)
+    println("FINISHED TASK   >>> $taskName")
 }
